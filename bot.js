5744c992a59a01375673d1bc5f15346c7746879a
ammar-rasheed@hotmail.com <ammar-rasheed@hotmail.com>
2023-09-11 16:51:52 +0300

    add check bot te;egram

---
+++ bot.js
@@ -0,0 +1,50 @@
+const TelegramBot = require('node-telegram-bot-api');
+const ThreeCommasAPI = require('3commas-api-node');
+
+const botToken = '6445954804:AAHpzEgRjQSm09obmx1KzX2l0dJdywgLsqY'; // Replace with your actual Bot token
+const chatId = -10019218085; // Replace with your actual chat ID
+
+const bot = new TelegramBot(botToken, { polling: true });
+
+const threeCommasAPI = new ThreeCommasAPI({
+ apiKey: '35c1fd4032924b3aad132a6d135294f8192816f541da44039dcea439f3e6cce3',
+  apiSecret: '25d7622eb5df82eace388295ab8872cdd6b6ea89dfb66bd31dde8ae26cd904c9f32511bdb166baa5b3ac8ddfb181d79e838636ec6baf929a454ad1e96d67e5ffa8d98091d9d51b044122c43638ea0a473688e48832d3a28ba81777bf965a23c5d37b1825',
+});
+
+// Listen for the /balance command
+bot.onText(/\/balance (.+)/, async (msg, match) => {
+  const chatId = msg.chat.id;
+  const userId = msg.from.id;
+
+
+  const accountArg = match[1]; // Get the argument provided after /balance command
+// Check if the user provided a valid account ID
+if (!accountArg || !/^\d+$/.test(accountArg)) {
+  bot.sendMessage(chatId, 'Please provide a valid 3Commas account ID. Example usage: /balance 12345');
+  return;
+}
+
+const userAccountId = accountArg;
+
+try {
+  // Fetch the account's balance
+  const account = await threeCommasAPI.accountLoadBalances(userAccountId);
+
+  // Extract and format the balance information
+  const balanceInfo = account?.primary_display_currency_amount || { amount: 0, currency: '' };
+  const formattedBalance = `${balanceInfo.amount} ${balanceInfo.currency}`;
+
+  // Send the balance to the user
+  bot.sendMessage(chatId, `Balance for account ${userAccountId}: ${formattedBalance}`);
+} catch (error) {
+  console.error('Error fetching account balance:', error);
+  bot.sendMessage(chatId, 'An error occurred while fetching the account balance.');
+}
+});
+
+// Start the bot
+bot.on('polling_error', (error) => {
+console.error('Polling error:', error);
+});
+
+
---
+++ check.js
@@ -0,0 +1,109 @@
+const TelegramBot = require('node-telegram-bot-api');
+const ThreeCommasAPI = require('3commas-api-node');
+
+const botToken = '6445954804:AAHpzEgRjQSm09obmx1KzX2l0dJdywgLsqY'; // Replace with your actual Bot token
+const alertChatId = '1188152105' ; // Chat ID to send alerts
+const resultsChatId = '1188152105'; // Chat ID to send balance check results
+
+const ALERT_THRESHOLD_PERCENT = 2;
+
+const bot = new TelegramBot(botToken, { polling: true });
+
+const threeCommasAPI = new ThreeCommasAPI({
+  apiKey: '35c1fd4032924b3aad132a6d135294f8192816f541da44039dcea439f3e6cce3',
+  apiSecret: '25d7622eb5df82eace388295ab8872cdd6b6ea89dfb66bd31dde8ae26cd904c9f32511bdb166baa5b3ac8ddfb181d79e838636ec6baf929a454ad1e96d67e5ffa8d98091d9d51b044122c43638ea0a473688e48832d3a28ba81777bf965a23c5d37b1825',
+
+});
+
+
+
+// Define API IDs and capitalMap
+const apiIds = [
+  32101201, 31876293, 32103676, 32178454, 32427154,32152427,
+      32427107, 32428979, 32433201, 31814867,32101635,32470971,32427159,32474224,
+];
+
+const capitalMap = new Map([
+  [31814867, { title: 'G13 ',strategy: '2X 15M 0.58%', capital: 1000 }],
+  [32103676, { title: 'G14 ',strategy: '2X 15M 0.58', capital: 2000 }],
+  [32474224,{title: 'G126 ',strategy: '2X 15M 0.58%',capital: 1000}], 
+  
+  [32470971,{title: 'G128 ',strategy: '2X 1H 1%',capital: 1000}], 
+  [32427154, { title: 'G124 ',strategy: '2X 1H 1%', capital: 1000 }],
+
+  [32427159,{title: 'G125 ',strategy: '2X 4H 1%',capital: 1000}], 
+  [32101201, { title: 'G9 ',strategy: '2X 4H 1%', capital: 1000 }],
+
+  [31876293, { title: 'G11 ',strategy: '4X 15M 0.58%', capital: 1000 }],
+  [32244371, { title: 'G118 ',strategy: '4x 15M 0.58%', capital: 1000 }],
+  [ 32101635,{title: 'G16 ',strategy: '4X 15M 1% ',capital: 1000}], 
+  [32476763,{title: 'G129 ',strategy: '4X 15M 1%',capital: 1000}], 
+
+  [32244363, { title: 'G117 ',strategy: '4X 1H 1%', capital: 1000 }],
+  [32260429, { title: 'G30 ',strategy: '4X 1H 1%', capital: 1000 }],
+  [32152427, { title: 'G22 ',strategy: '4X 1H 0.58%', capital: 1000 }],
+
+  
+
+  
+  [32178454, { title: 'G24',strategy: '', capital: 1000 }],
+  [32427107, { title: 'G122',strategy: '', capital: 1700 }],
+  [32428979, { title: 'G66+1000',strategy: '', capital: 3000 }],
+  [32433201, { title: 'G67', strategy: '',capital: 345 }],
+  [32423630, { title: 'G98',strategy: '', capital: 427 }],
+  [32435532, { title: 'G72+600', strategy: '',capital: 2600 }],
+]);
+
+
+// Function to send a notification
+async function sendAlert(accountId, title, percentage) {
+  try {
+    // Send an alert message to the specified chat ID
+    await bot.sendMessage(alertChatId, `Alert: Account ${accountId} (${title}) exceeded ${ALERT_THRESHOLD_PERCENT}% change: ${percentage}%`);
+  } catch (error) {
+    console.error('Error sending alert to Telegram bot:', error);
+  }
+}
+
+// Define the checkBalances function
+async function checkBalances() {
+  try {
+    // Fetch balances for each account ID
+    const results = await Promise.all(apiIds.map(async (id) => {
+      const account = await threeCommasAPI.accountLoadBalances(id);
+      const capitalInfo = capitalMap.get(id);
+      const balance = Math.floor(account?.primary_display_currency_amount?.amount) || 0;
+      const capital = capitalInfo?.capital || 0;
+      const percentage = ((balance - capital) / capital * 100).toFixed(1);
+
+      // Check if the percentage exceeds the alert threshold
+      if (percentage >= ALERT_THRESHOLD_PERCENT) {
+        sendAlert(id, capitalInfo?.title || '', percentage);
+      }
+
+      return { id, title: capitalInfo?.title || '', percentage };
+    }));
+
+    // Log the results or perform any desired actions
+    console.log('Balance check results:', results);
+
+    // Send the balance check results to the results chat ID
+    await bot.sendMessage(resultsChatId, 'Balance check results:');
+    for (const result of results) {
+      await bot.sendMessage(resultsChatId, `Account ID: ${result.id}, Title: ${result.title}, Percentage: ${result.percentage}%`);
+    }
+  } catch (error) {
+    console.error('Error checking balances:', error);
+  }
+}
+
+// Handle /check command
+bot.onText(/\/check/, (msg) => {
+  const chatId = msg.chat.id;
+  console.log('Chat ID:', chatId); // Print the chat ID to the console
+
+  bot.sendMessage(chatId, 'Checking balances...');
+
+  // Call the function to check balances and send alerts for api1
+  checkBalances();
+});
