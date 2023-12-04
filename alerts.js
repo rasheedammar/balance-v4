4f138a33f52824ca83b298595437b27df9ce5acd
Ammar Rasheed <32423461+rasheedammar@users.noreply.github.com>
2023-12-04 17:45:24 +0300

    Update alerts.js balance groups

--- alerts.js
+++ alerts.js
@@ -30,41 +30,40 @@ const threeCommasAPI2 = new ThreeCommasAPI({
 
 // Define API IDs and capitalMap
 const api1Ids  = [
-  32101201, 31876293, 32103676, 32178454, 32427154,32152427,
-  32427107, 32428979, 32433201, 31814867,32101635,32470971,32474224,
+32103674,32427154,32152427,32428979,32103676,32101635,
 ];
 const api2Ids  = [
-  32244363, 32244371,32423630, 32435532,32260429,32476763,
+  32244371, 32435532, 32260429, 32244363,
 ];
 const capitalMap = new Map([
-  [31814867, { title: 'G13 ',strategy: '2X 15M 0.58%', capital: 1000 }],
-  [32103676, { title: 'G14 ',strategy: '2X 15M 0.58', capital: 2000 }],
-  [32474224,{title: 'G126 ',strategy: '2X 15M 0.58%',capital: 1000}], 
-  
-  [32470971,{title: 'G128 ',strategy: '2X 1H 1%',capital: 1000}], 
-  //[32427154, { title: 'G124 ',strategy: '2X 1H 1%', capital: 1000 }],
-[32427154, { title: 'G124+300 hs17 +500 b24 ',strategy: '2X 1H 1%', capital: 1000+300+500 }],
-  //to 129 [32427159,{title: 'G125 ',strategy: '2X 4H 1%',capital: 1000}], 
-  [32101201, { title: 'G9 ',strategy: '2X 4H 1%', capital: 1000 }],
 
-  [31876293, { title: 'G11 ',strategy: '4X 15M 0.58%', capital: 1000 }],
-  [32244371, { title: 'G118 ',strategy: '4x 15M 0.58%', capital: 1000 }],
-  [ 32101635,{title: 'G16 ',strategy: '4X 15M 1% ',capital: 1000}], 
-  [32476763,{title: 'G129 +1000',strategy: '4X 15M 1%',capital: 2000}], 
-
-  [32244363, { title: 'G117 ',strategy: '4X 1H 1%', capital: 1000 }],
-  [32260429, { title: 'G30 ',strategy: '4X 1H 1%', capital: 1000 }],
+  [32427154, { title: 'G124 ',strategy: '2X 1H 1%', capital: 1600 }],
+  [32244371, { title: 'G118 ',strategy: '4x 15M 0.58%', capital: 2300 }],
+  [32244363, { title: 'G117 ',strategy: '4X 1H 1%', capital: 3026 }],
+  [32260429, { title: 'G30 ',strategy: '4X 1H 1%', capital: 1900 }],
   [32152427, { title: 'G22 ',strategy: '4X 1H 0.58%', capital: 1000 }],
-
+  [32428979, { title: 'G66',strategy: '', capital: 1360 }],
+  [32435532, { title: 'G72+600', strategy: '',capital: 500 }],
+  [32103674, { title: 'G14 ',strategy: 'spot', capital: 2040 }],
+  [32103676, { title: 'G14 ',strategy: 'Future', capital: 1007 }],
+  [32101635,{title: 'G16 ',strategy: '4X 15M 1% ',capital: 1000}], 
   
 
+
   
-  [32178454, { title: 'G24',strategy: '', capital: 1000 }],
-  [32427107, { title: 'G122',strategy: '', capital: 1700 }],
-  [32428979, { title: 'G66+1000',strategy: '', capital: 3000 }],
-  [32433201, { title: 'G67', strategy: '',capital: 345 }],
-  [32423630, { title: 'G98',strategy: '', capital: 427 }],
-  [32435532, { title: 'G72+600', strategy: '',capital: 2600 }],
+  // [31814867, { title: 'G13 ',strategy: '2X 15M 0.58%', capital: 1000 }],
+  // [32474224,{title: 'G126 ',strategy: '2X 15M 0.58%',capital: 1000}], 
+  // [32470971,{title: 'G128 ',strategy: '2X 1H 1%',capital: 1000}], 
+  //[32427154, { title: 'G124 ',strategy: '2X 1H 1%', capital: 1000 }],
+  //to 129 [32427159,{title: 'G125 ',strategy: '2X 4H 1%',capital: 1000}], 
+  // [32101201, { title: 'G9 ',strategy: '2X 4H 1%', capital: 1000 }],
+  //  [31876293, { title: 'G11 ',strategy: '4X 15M 0.58%', capital: 1000 }],
+  // [32476763,{title: 'G129 +1000',strategy: '4X 15M 1%',capital: 2000}], 
+  //[32178454, { title: 'G24',strategy: '', capital: 1000 }],
+  // [32427107, { title: 'G122',strategy: '', capital: 1700 }],
+  // [32433201, { title: 'G67', strategy: '',capital: 345 }],
+  //[32423630, { title: 'G98',strategy: '', capital: 427 }],
+
 ]);
 
 
